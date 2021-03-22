4f5e5aa1a9d5fd00de5fbfabf3b97220ff2204f7
demetkochan <61080306+demetkochan@users.noreply.github.com>
2021-03-22 14:01:30 +0300

    sentiment

---
+++ sentiment/server.py
@@ -0,0 +1,19 @@
+import joblib
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+load_vec = joblib.load("tfidf_vector.sav")
+lr_model = joblib.load("logistic_reg_model.sav")
+
+@app.route('/sentiment', methods =['GET','POST'])
+def get_prediction():
+    text = request.json.get('text')
+    vec = load_vec.transform([text])
+
+    return jsonify({"result": lr_model.predict(vec)[0]})
+
+if __name__ =="__main__":
+    app.run(debug=True,host="0.0.0.0")
+
+
