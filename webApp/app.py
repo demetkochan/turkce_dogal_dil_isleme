54d5ca1caf0feb4aea45b8bc17c9a571601ba41e
beyza <beyzacanbay34@gmail.com>
2021-04-20 11:05:45 +0300

    Add Web Serves

--- webApp/app.py
+++ webApp/app.py
@@ -7,11 +7,51 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route("/",methods=["GET"])
 def home():
-    return render_template("index.html")
+    return render_template("SignPage.html")
 
 @app.route("/sentiment", methods=['POST'])
 def sentiment():
     url =  "http://localhost:5000/sentiment"
+    payload = {"text": request.form["input_text"]}
+    response = json.loads(requests.request("POST", url, json=payload).text)
+
+    return jsonify(response)
+
+@app.route("/kelimecikarma", methods=['POST'])
+def keywordExtraction():
+    url =  "http://localhost:5000/keyword"
+    payload = {"text": request.form["input_text"],"text": request.form["input_text"]}
+    response = json.loads(requests.request("POST", url, json=payload).text)
+
+    return jsonify(response)
+
+@app.route("/konutanimlama", methods=['POST'])
+def categorization():
+    url =  "http://localhost:5000/categorization"
+    payload = {"text": request.form["input_text"]}
+    response = json.loads(requests.request("POST", url, json=payload).text)
+
+    return jsonify(response)
+
+@app.route("/sorucevap", methods=['POST'])
+def questionAnswer():
+    url =  "http://localhost:5000/questionAns"
+    payload = {"text": request.form["input_text"],"text": request.form["input_text"]}
+    response = json.loads(requests.request("POST", url, json=payload).text)
+
+    return jsonify(response)
+
+@app.route("/ozetleme", methods=['POST'])
+def summarization():
+    url =  "http://localhost:5000/summarization"
+    payload = {"text": request.form["input_text"]}
+    response = json.loads(requests.request("POST", url, json=payload).text)
+
+    return jsonify(response)
+
+@app.route("/varliktanima", methods=['POST'])
+def ner():
+    url =  "http://localhost:5000/ner"
     payload = {"text": request.form["input_text"]}
     response = json.loads(requests.request("POST", url, json=payload).text)
 
