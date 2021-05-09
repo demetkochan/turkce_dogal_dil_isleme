9740127afb386016c20c4a057ff76e0ef3c2b24d
ilaydagler <ilydagulr@gmail.com>
2021-05-09 14:32:32 +0300

    test

--- Django_wordMixer/wordmixer/wordmixer/urls.py
+++ Django_wordMixer/wordmixer/wordmixer/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from wordmixer.blog.views import file_upload
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
